f4d70286d8f0d0932b5d1324a2a6e97e8f465ec5
YassSSH <yassine.regragui@mediaschool.me>
2022-02-17 14:25:42 +0100

    Page Panier

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from "./styles"
+import CartItem from './CartItem/CartItem';
 
 const Cart = ( { cart }) => {
 
-    const isEmpty = cart.line_items.length === 0
     const classes = useStyles(); 
 
     const EmptyCart = () => (
@@ -16,7 +16,7 @@ const Cart = ( { cart }) => {
         <Grid container spacing={3}>
             {cart.line_items.map((item) => (
                 <Grid item xs={12} sm={4} key={item.id} >
-                    <div>{item.name}</div>
+                    <CartItem item={item} />
                 </Grid>
 
             ))}
@@ -29,7 +29,7 @@ const Cart = ( { cart }) => {
                     <Button className={classes.emptyButton} size="large" type="Button" variant="contained" color='secondary'>
                         Vider le panier
                     </Button>
-                    <Button className={classes.checkOutButton} size="large" type="Button" variant="contained" color='primary'>
+                    <Button className={classes.checkoutButton} size="large" type="Button" variant="contained" color='primary'>
                         Payer !
                     </Button>
                 </div>
@@ -38,12 +38,14 @@ const Cart = ( { cart }) => {
         </>
     )
 
+    if(!cart.line_items) return 'Loading...'
+
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3"> Votre Panier</Typography>
+            <Typography className={classes.title} variant="h3" gutterBottom> Votre Panier</Typography>
 
-            {isEmpty ? <EmptyCart /> : <FilledCart />}
+            {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     );
 };
